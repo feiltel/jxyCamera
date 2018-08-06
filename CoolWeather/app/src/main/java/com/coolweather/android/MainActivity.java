874341db26acd157385c7925aaf6317ddaf92d19
feiltel <wy871139697@163.com>
2018-08-06 09:58:14 +0800

    引入Android——log4j

--- CoolWeather/app/src/main/java/com/coolweather/android/MainActivity.java
+++ CoolWeather/app/src/main/java/com/coolweather/android/MainActivity.java
@@ -3,7 +3,10 @@ package com.coolweather.android;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.coolweather.android.log.ConfigureLog4J;
 import com.coolweather.android.log.CoolWeatherLogger;
+
+import org.apache.log4j.Logger;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +14,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        CoolWeatherLogger.getCoolWeatherLogger().info("queryProvinces: ");
-        CoolWeatherLogger.getCoolWeatherLogger().debug("queryProvinces: ");
-        CoolWeatherLogger.getCoolWeatherLogger().error("queryProvinces: ");
-       // setContentView(R.layout.activity_main);
+
+
+        ConfigureLog4J configureLog4J=new ConfigureLog4J();
+        configureLog4J.configure();
+        //初始化 log
+
+        Logger log=Logger.getLogger(this.getClass());
+        //写 info 日志
+        log.info("不知道呀就是测试一下啊");
 
     }
 }
--- CoolWeather/app/src/main/java/com/coolweather/android/log/CoolWeatherLogger.java
+++ CoolWeather/app/src/main/java/com/coolweather/android/log/CoolWeatherLogger.java
@@ -17,107 +17,117 @@ public class CoolWeatherLogger extends Logger {
 
     protected CoolWeatherLogger(String name) {
         super(name);
-        this.module=name;
-        this.feature=name;
-        logger=Logger.getLogger(name);
+        this.module = name;
+        this.feature = name;
+        logger = Logger.getLogger(name);
     }
-    public Logger getLogger(){
+
+    public Logger getLogger() {
         return getLogger("CoolWeatherLogger");
     }
-    public static synchronized CoolWeatherLogger getLogger(String name){
-        if (coolWeatherLogger==null){
-            coolWeatherLogger=new CoolWeatherLogger(name);
+
+    public static synchronized CoolWeatherLogger getLogger(String name) {
+        if (coolWeatherLogger == null) {
+            coolWeatherLogger = new CoolWeatherLogger(name);
         }
-        className=name;
+        className = name;
         return coolWeatherLogger;
     }
-    public static synchronized CoolWeatherLogger getCoolWeatherLogger(){
-        if (coolWeatherLogger==null){
-            coolWeatherLogger=new CoolWeatherLogger("CoolWeatherLogger");
+
+    public static synchronized CoolWeatherLogger getCoolWeatherLogger() {
+        if (coolWeatherLogger == null) {
+            coolWeatherLogger = new CoolWeatherLogger("CoolWeatherLogger");
         }
         return coolWeatherLogger;
     }
+
     private PatternLayout patternLayout;
     private Logger logger;
-    private void  setAppender(String format,HelpAppender fileAppender ){
-        if (fileAppender==null||this.patternLayout==null){
+
+    private void setAppender(String format, HelpAppender fileAppender) {
+        if (fileAppender == null || this.patternLayout == null) {
             return;
         }
         logger.removeAllAppenders();
         patternLayout.setConversionPattern(format);
         logger.addAppender(fileAppender);
-
     }
-    private String OPERATION_FORMAT="%d%p%m";
-    private String EXCEPTION_FORMAT="%d%p%m";
+
+    private String OPERATION_FORMAT = "%d%p%m";
+    private String EXCEPTION_FORMAT = "%d%p%m";
     private HelpAppender debugLogerAppender;
     private HelpAppender infoLogerAppender;
     private HelpAppender errorLogerAppender;
+
     @Override
     public void debug(Object message) {
-        if (message==null){
+        if (message == null) {
             return;
         }
-        setAppender(OPERATION_FORMAT,debugLogerAppender);
+        setAppender(OPERATION_FORMAT, debugLogerAppender);
         logger.debug(newMessage(message));
     }
 
     private String newMessage(Object message) {
-        String newMsg=message.toString();
-        if (message instanceof Throwable){
-            newMsg =((Throwable) message).getMessage();
+        String newMsg = message.toString();
+        if (message instanceof Throwable) {
+            newMsg = ((Throwable) message).getMessage();
         }
-        return String.format(PATTERN,module,feature,className,newMsg);
+
+        return String.format(PATTERN, module, feature, className, newMsg);
     }
+
     private String newMessage(Throwable message) {
         return message.getMessage();
     }
+
     @Override
     public void info(Object message) {
-        if (message==null){
+        if (message == null) {
             return;
         }
-        setAppender(OPERATION_FORMAT,infoLogerAppender);
+        setAppender(OPERATION_FORMAT, infoLogerAppender);
         logger.info(newMessage(message));
     }
 
     @Override
     public void error(Object message) {
-        if (message==null){
+        if (message == null) {
             return;
         }
-        setAppender(EXCEPTION_FORMAT,errorLogerAppender);
-        String newMsg=message.toString();
-        if (message instanceof Throwable){
-            newMsg=newMessage((Throwable)message);
+        setAppender(EXCEPTION_FORMAT, errorLogerAppender);
+        String newMsg = message.toString();
+        if (message instanceof Throwable) {
+            newMsg = newMessage((Throwable) message);
 
         }
-        setAppender(OPERATION_FORMAT,errorLogerAppender);
+        setAppender(OPERATION_FORMAT, errorLogerAppender);
 
-        logger.error(String.format(PATTERN,module,feature,className,newMsg));
+        logger.error(String.format(PATTERN, module, feature, className, newMsg));
     }
 
     @Override
     public void error(Object message, Throwable t) {
-        if (message==null||t==null){
+        if (message == null || t == null) {
 
             return;
         }
-        setAppender(OPERATION_FORMAT,errorLogerAppender);
-        logger.error(newMessage(message+": "+t.getMessage()));
-        setAppender(EXCEPTION_FORMAT,errorLogerAppender);
-        logger.error(newMessage(message+newMessage(t)));
+        setAppender(OPERATION_FORMAT, errorLogerAppender);
+        logger.error(newMessage(message + ": " + t.getMessage()));
+        setAppender(EXCEPTION_FORMAT, errorLogerAppender);
+        logger.error(newMessage(message + newMessage(t)));
 
     }
-    public void setPath(String sdPath){
-        this.sdPath=sdPath;
-        String infoLogPath=sdPath+"/"+"log/common/info/info.log";
-        String errorLogPath=sdPath+"/"+"log/common/error/error.log";
-        String debugLogPath=sdPath+"/"+"log/common/debug/debug.log";
-        this.infoLogerAppender=new HelpAppender(infoLogPath);
-        this.errorLogerAppender=new HelpAppender(errorLogPath);
-        this.debugLogerAppender=new HelpAppender(debugLogPath);
-        this.patternLayout=new PatternLayout();
+
+    public void setPath(String sdPath) {
+        this.sdPath = sdPath;
+        String infoLogPath = sdPath + "/" + "log/common/info/info.log";
+        String errorLogPath = sdPath + "/" + "log/common/error/error.log";
+        String debugLogPath = sdPath + "/" + "log/common/debug/debug.log";
+        this.infoLogerAppender = new HelpAppender(infoLogPath);
+        this.errorLogerAppender = new HelpAppender(errorLogPath);
+        this.debugLogerAppender = new HelpAppender(debugLogPath);
+        this.patternLayout = new PatternLayout();
 
     }
 }
