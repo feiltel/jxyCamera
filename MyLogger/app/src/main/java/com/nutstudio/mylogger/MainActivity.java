100bf4cb05ceb0ecec9e223f426649ed21b21486
feiltel <wy871139697@163.com>
2018-08-07 12:02:08 +0800

    完善配置文件 清理无用代码

--- MyLogger/app/src/main/java/com/nutstudio/mylogger/MainActivity.java
+++ MyLogger/app/src/main/java/com/nutstudio/mylogger/MainActivity.java
@@ -1,25 +1,30 @@
 package com.nutstudio.mylogger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.nutstudio.mylogger.logutils.Log;
+import com.nutstudio.mylogger.logutils.Log4jConfigure;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log4jConfigure.configure();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.getLogger(MainActivity.class).debug("MainActivity1");
+        Log.getLogger(MainActivity.class).error("MainActivity1");
+        Log.getLogger(MainActivity.class).info("MainActivity1");
 
         findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("yyy","MainActivity");
-                Log.d("yyy","MainActivity");
-                Log.e("yyy","MainActivity");
-                Log.getLogger(MainActivity.class).debug("MainActivity");
+                Log4jConfigure.configure(true, Log4jConfigure.My_LOG_DIR);
+                Log.getLogger(MainActivity.class).debug("MainActivity1");
+                Log.getLogger(MainActivity.class).error("MainActivity1");
+                Log.getLogger(MainActivity.class).info("MainActivity1");
             }
         });
     }
--- MyLogger/app/src/main/java/com/nutstudio/mylogger/logutils/Log4jConfigure.java
+++ MyLogger/app/src/main/java/com/nutstudio/mylogger/logutils/Log4jConfigure.java
@@ -1,48 +1,40 @@
 
 package com.nutstudio.mylogger.logutils;
 
-import java.io.File;
-
-
-
 import android.os.Environment;
 
 import com.nutstudio.mylogger.log.LogConfigurator;
 
 import org.apache.log4j.Level;
 
+import java.io.File;
+
 
 public class Log4jConfigure {
-    private static final int MAX_FILE_SIZE = 1024 * 1024 * 10;
-    private static final String DEFAULT_LOG_DIR = "//MyApp//Log//";
-    private static final String DEFAULT_LOG_FILE_NAME = "MyApp.log";
+    public static final String My_LOG_DIR = "//MyApp//MyLog//info.log";
+    public static final String DEFAULT_LOG_DIR = "//MyApp//DEFAULT//info.log";
     private static final String TAG = "Log4jConfigure";
     // 对应AndroidManifest文件中的package
-    private static final String PACKAGE_NAME = "com.king.logtest";
-
-    public static void configure(String fileName) {
+    public static void configure(boolean isDebug, String filePath) {
         final LogConfigurator logConfigurator = new LogConfigurator();
         try {
             if (isSdcardMounted()) {
                 logConfigurator.setFileName(Environment.getExternalStorageDirectory()
-                        + DEFAULT_LOG_DIR + fileName);
-            } else {
-                logConfigurator.setFileName("//data//data//" + PACKAGE_NAME + "//files"
-                        + File.separator + fileName);
+                        + File.separator + filePath);
             }
             //以下设置是按指定大小来生成新的文件
             /*
              * logConfigurator.setMaxBackupSize(4);
              * logConfigurator.setMaxFileSize(MAX_FILE_SIZE);
              */
-            
+
             //以下设置是按天生成新的日志文件,与以上两句互斥,MAX_FILE_SIZE将不在起作用
             //文件名形如 MyApp.log.2016-06-02,MyApp.log.2016-06-03
             logConfigurator.setUseDailyRollingFileAppender(true);
-            
+
             //以下为通用配置
             logConfigurator.setImmediateFlush(true);
-            logConfigurator.setRootLevel(Level.DEBUG);
+            logConfigurator.setRootLevel(isDebug ? Level.DEBUG : Level.INFO);
             logConfigurator.setFilePattern("%d\t%p/%c:\t%m%n");
             logConfigurator.configure();
             android.util.Log.e(TAG, "Log4j config finish");
@@ -53,7 +45,7 @@ public class Log4jConfigure {
     }
 
     public static void configure() {
-        configure(DEFAULT_LOG_FILE_NAME);
+        configure(false, DEFAULT_LOG_DIR);
     }
 
     private static boolean isSdcardMounted() {
